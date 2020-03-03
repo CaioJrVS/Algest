711ef828d94ffd2eec987e582f0b605e5a4adeb3
CaioJrVS <caio.junior.vs@gmail.com>
2020-03-03 20:37:39 -0300

    .

--- c/linkedlist.c
+++ c/linkedlist.c
@@ -11,6 +11,15 @@ struct node *create_ll(struct node *);
 struct node *display(struct node *);
 struct node *insert_beg(struct node *);
 struct node *insert_end(struct node *);
+struct node *insert_before(struct node *);
+struct node *insert_after(struct node *);
+struct node *delete_beg(struct node *);
+struct node *delete_end(struct node *);
+struct node *delete_node(struct node *);
+struct node *delete_after(struct node *);
+struct node *delete_list(struct node *);
+struct node *sort_list(struct node *);
+
 
 int main (){
 int num;
@@ -20,6 +29,14 @@ do{
     printf("\nChoose 2 to display the linked list");
     printf("\nChoose 3 to add a node at the beginning");
     printf("\nChoose 4 to add a node at the end");
+    printf("\nChoose 5 to insert node before a node");
+    printf("\nChoose 6 to insert a node after a node");
+    printf("\nChoose 7 to delete beginning node");
+    printf("\nChoose 8 to delete last node");
+    printf("\nChoose 9 to delete chosen node");
+    printf("\nChoose 10 to delete after node");
+    printf("\nChoose 11 to delete list");
+    printf("\nChoose 12 to sort list");
     printf("\n\nEnter your option\n");
     scanf("%d", &num);
     switch (num) {
@@ -31,6 +48,22 @@ do{
 	case 3: start = insert_beg(start);
 		break;
 	case 4: start = insert_end(start);
+		break;
+	case 5: start = insert_before(start);
+		break;
+	case 6: start = insert_after(start);
+		break;
+	case 7: start = delete_beg(start);
+		break;
+	case 8: start = delete_end(start);
+		break;
+	case 9: start = delete_node(start);
+		break;
+	case 10: start = delete_after(start);
+		break;
+	case 11: start = delete_list(start);
+		break;
+	case 12: start = sort_list(start);
 		break;
     }
 }while(num !=13);
@@ -96,4 +129,8 @@ struct node *insert_end(struct node *start){
 	ptr = ptr->next;
     ptr -> next = new_node;
     return start;
+};
+
+struct node *insert_before(struct node start){
+    struct node 
 };
